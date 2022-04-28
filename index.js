b1869da3382373de1416d72909f8bfe361f3d7a5
aaron592 <64913505+aaron592@users.noreply.github.com>
2022-04-28 14:07:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const {movieModel,actorModel,bothModel} = require('./schema-model');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 //middleware 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended:true}));
 const PORT = process.env.PORT || 3000;
 
 //mongodb connection
-mongoose.connect("mongodb+srv://ATJ:Atj@cluster0.onn2g.mongodb.net/moviesDB?retryWrites=true&w=majority",{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology:true
 
